e14425ebff1760143f67a47f8a9e5041f0a02c3d
paivav <paivav@pge.rj.gov.br>
2023-12-14 13:03:21 -0300

    configurando git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,5 +7,10 @@ pipeline {
                 echo 'Hello World'
             }
         }
+        stage('git') {
+            steps {
+                git branch: 'main', url: 'https://github.com/abhishekpatil2006/abhishekpatil2006.git'
+            
+        }
     }
 }
