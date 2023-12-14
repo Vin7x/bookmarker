91be1f0244236d88564c1cba94669a5255cd1218
paivav <paivav@pge.rj.gov.br>
2023-12-14 16:31:56 -0300

    opa!

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -35,10 +35,10 @@ pipeline {
             steps {
                 script {
                     // Instalação das dependências do npm (se ainda não foi feita)
-                    sh 'npm install'
+                    bat 'npm install'
 
                     // Execução do arquivo .side usando o Selenium IDE Runner
-                    sh 'selenium-side-runner -c "chrome" -p 4444 -u http://localhost:4444/wd/hub -w 10 /tests/TestCase/googleTest.side'
+                    bat 'selenium-side-runner -c "chrome" -p 4444 -u http://localhost:4444/wd/hub -w 10 /tests/TestCase/googleTest.side'
                 }
             }
         }
