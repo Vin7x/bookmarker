e2de81ec6f30f11c8a9f05226b6fa5fc8c5893fe
paivav <paivav@pge.rj.gov.br>
2023-12-14 13:13:13 -0300

    git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ pipeline {
         }
         stage('git') {
             steps {
-                git branch: 'main', url: 'https://github.com/abhishekpatil2006/abhishekpatil2006.git'
+                git branch: 'dev', url: 'https://github.com/Vin7x/bookmarker.git'
             
             }
          }
