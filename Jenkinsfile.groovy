d593a67bc6cef4d04405da870c94a616ba3bf52a
paivav <paivav@pge.rj.gov.br>
2023-12-14 15:50:08 -0300

    mudando pra sh

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -34,7 +34,7 @@ pipeline {
         stage('Run Selenium Tests') {
             steps {
                 script {
-                    bat 'vendor\\bin\\phpunit tests\\GoogleTest.php' //arquivo de testes
+                    sh 'vendor\\bin\\phpunit tests\\GoogleTest.php' //arquivo de testes
                 }
             }
         }
