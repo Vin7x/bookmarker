8f884f3ba55d1859b9e956cef80a8982760162ba
paivav <paivav@pge.rj.gov.br>
2023-12-14 14:53:09 -0300

    configurando selenium e setando arquivo de teste

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,11 +7,36 @@ pipeline {
                 echo 'Hello World'
             }
         }
+
         stage('git') {
             steps {
                 git branch: 'dev', url: 'https://github.com/Vin7x/bookmarker.git'
                 echo 'Versionamento funcionando!'
             }
-         }
+        }
+
+        stage('Install Dependencies') {
+            steps {
+                script {
+                    sh 'npm install selenium-webdriver'
+                }
+            }
+        }
+
+        stage('Configure Selenium') {
+            steps {
+                script {
+                    def driver = new org.openqa.selenium.chrome.ChromeDriver()
+                }
+            }
+        }
+
+        stage('Run Selenium Tests') {
+            steps {
+                script {
+                    sh 'node google.spec.js' //arquivo de testes
+                }
+            }
+        }
     }
 }
