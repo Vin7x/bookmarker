37be099c6238e5094aea969f9f2426076b7bc965
paivav <paivav@pge.rj.gov.br>
2023-12-17 16:21:53 -0300

    asdas

--- tests/TestCase/login.js
+++ tests/TestCase/login.js
@@ -25,7 +25,6 @@ const assert = require('assert');
     // assert.deepStrictEqual(value, "Selenium")
 
     // await driver.quit();
-    print("Login realizado com sucesso");
   } catch (error) {
     print(error);
   }
