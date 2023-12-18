582bc083345bc041c7036ca6c96f44f23be98ef9
paivav <paivav@pge.rj.gov.br>
2023-12-18 10:16:12 -0300

    teste sem instalar webdriver

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -28,7 +28,7 @@ pipeline {
                 echo "executando selenium-side-runner"
                 bat script: "selenium-side-runner tests/TestCase/GoogleTest.side || exit 0"
                 echo "executando selenium via node"
-                bat script: "npm install selenium-webdriver"
+                // bat script: "npm install selenium-webdriver"
                 bat script: "node tests/TestCase/login.js || exit 0"
             }
         }
