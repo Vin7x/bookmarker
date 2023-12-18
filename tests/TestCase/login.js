67ba63ef3360ed47553cb1f048889ea28dab889a
paivav <paivav@pge.rj.gov.br>
2023-12-18 13:11:10 -0300

    testes

--- tests/TestCase/login.js
+++ tests/TestCase/login.js
@@ -1,43 +1,48 @@
-const {Builder, By} = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 const assert = require('assert');
 
 (async function firstScript() {
+  let driver;
+
   try {
-    let driver = await new Builder()
-    .forBrowser('chrome')
-    .build();
+    // Configurando opções do Chrome
+    const chromeOptions = new chrome.Options()
+      .addArguments('--no-sandbox')
+      .addArguments('--disable-dev-shm-usage')
+      .addArguments('--disable-setuid-sandbox')
+      .addArguments('--disable-extensions')
+      .headless();
+
+    driver = await new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(chromeOptions)
+      .build();
 
     await driver.get('http://10.120.100.168/paivav/pgedigital/processo-administrativo');
     console.log('Navegador aberto com sucesso!');
 
-    await driver.getTitle();
-    console.log('Tela carregada com sucesso!');
-
-    await driver.manage().setTimeouts({implicit: 1000})
+    // Ajuste no setTimeouts
+    await driver.manage().setTimeouts({ pageLoad: 10000 }); // 10 segundos
 
     let login = await driver.findElement(By.name('login'));
     let senha = await driver.findElement(By.name('senha'));
     let Button = await driver.findElement(By.className('btn btn-lg btn-primary btn-block'));
 
-    await login.sendKeys('paivav');
+    await login.sendKeys('testepgedigital');
     console.log('Login efetuado com sucesso!');
-    await senha.sendKeys('1122334455V!');
+
+    await senha.sendKeys('Senha1234');
     console.log('Senha efetuada com sucesso!');
+
     await Button.click();
 
-    // let value = await searchBox.getAttribute("value");
-    // assert.deepStrictEqual(value, "Selenium")
-
-    // await driver.quit();
-    console.log("Teste bem-sucedido!");
+    console.log('Teste bem-sucedido!');
   } catch (error) {
-    console.log(error);
-    if (error == "net::ERR_CONNECTION_TIMED_OUT") {
-      console.log("Erro no tempo de resposta com o servidor!");
-      console.log("Testando conexao com a Internet!");
-      await driver.get('http://www.google.com');
-    console.log('Navegador aberto com sucesso!');
-      
+    console.error('Ocorreu um erro:', error);
+  } finally {
+    if (driver) {
+      await driver.quit();
     }
   }
 })();
