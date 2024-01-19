f1bab3b221a24cc8462bf3562fb7dbab4ec4077e
paivav <paivav@pge.rj.gov.br>
2024-01-19 16:21:02 -0300

    asdasd

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -40,10 +40,5 @@ pipeline {
                  useWrapperFileDirectly: true]) 
             }
         }
-        post {
-            always {
-                chucknorris();
-            }
-        }
     }
 }
